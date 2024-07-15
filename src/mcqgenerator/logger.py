04569233a66f0cbea53e730e18a5de18e25aeead
sudhanshu2043 <sudhanshukrmfp@gmail.com>
2024-07-15 21:44:21 +0530

    updated logger file

--- src/mcqgenerator/logger.py
+++ src/mcqgenerator/logger.py
@@ -12,7 +12,6 @@ os.makedirs(log_path,exist_ok=True)
 
 LOG_FILEPATH=os.path.join(log_path,LOG_FILE)
 
-
 logging.basicConfig(level=logging.INFO, 
         filename=LOG_FILEPATH,
         format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
